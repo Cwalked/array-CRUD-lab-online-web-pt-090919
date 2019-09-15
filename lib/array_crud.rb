0ce21a8eb6d6128ce327be508d58446978ac4b75
Courtney Walker <cwalked@gmail.com>
2019-09-15 14:40:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["food", "air", "sleep"]
-  array << "water"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
